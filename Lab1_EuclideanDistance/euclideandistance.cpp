0445633954d5a608658fc01191b3aecff534ace6
nakanamohale <1174093@students.wits.ac.za>
2018-08-18 16:10:31 +0200

    Update euclideandistance.cpp

--- Lab1_EuclideanDistance/euclideandistance.cpp
+++ Lab1_EuclideanDistance/euclideandistance.cpp
@@ -22,19 +22,6 @@ void generate1D_Points(double *arrPoints, size_t n){
  */
 double MaxDistance1D(double *arrPoints, size_t n){
 	//TODO: write funcitonality for this method here
-	double max_dist = 0;
-	double dist=0;
-	for(int i=0; i<n; i++){
-		for(int j=i;j<n; j++){
-			double x1 = arrPoints[i];
-			double x2 = arrPoints[j];
-			dist = abs(x1-x2);
-			if(dist>max_dist){
-				max_dist = dist;
-			}
-		}
-	}
-	return max_dist;
 }
 
 
@@ -61,20 +48,6 @@ void generate2D_Points(point2D *arrPoints, size_t n){
  */
 double MaxDistance2D(point2D *arrPoints, size_t n){
 	//TODO: write funcitonality for this method here
-	
-	double max_dist = 0;
-	double dist=0;
-	for(int i=0; i<n; i++){
-		for(int j=i;j<n; j++){
-			point2D p1 = arrPoints[i];
-			point2D p2 = arrPoints[j];
-			dist = sqrt((p1.x-p2.x)*(p1.x-p2.x) + (p1.y-p2.y)*p1.x-p2.x);
-			if(dist>max_dist){
-				max_dist = dist;
-			}
-		}
-	}
-	return max_dist;
 }
 
 
@@ -101,19 +74,6 @@ void generate3D_Points(point3D *arrPoints, size_t n){
  */
 double MaxDistance3D(point3D *arrPoints, size_t n){
 	//TODO: write funcitonality for this method here
-	double max_dist = 0;
-	double dist=0;
-	for(int i=0; i<n; i++){
-		for(int j=i;j<n; j++){
-			point3D p1 = arrPoints[i];
-			point3D p2 = arrPoints[j];
-			dist = sqrt(pow(p1.x-p2.x,2) + pow(p1.y-p2.y,2) + pow(p1.z-p2.z,2));
-			if(dist>max_dist){
-				max_dist = dist;
-			}
-		}
-	}
-	return max_dist;
 }
 
 int main(void){
@@ -122,14 +82,6 @@ int main(void){
 	 *		3.) call the appropriate MaxDistance1D/2D/3D function to calculate the maximum distance
 	 *		4.) output the maximum distance to the terminal 
 	 */
-	int N=10;
-	point3D *arrPoints = new point3D[N];
-	generate3D_Points(arrPoints,N);
-	double maximum_dist = MaxDistance3D(arrPoints,N);
-	cout<<"Maximum Distance: "<<maximum_dist<<endl;
-	
-	free(arrPoints);
-	
 	return 0;
 }
 
